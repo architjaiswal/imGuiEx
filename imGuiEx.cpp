517b7622103600bfb89dbdbbfc09dacd6449b279
14077 <14077@MSI>
2022-07-07 18:20:29 -0500

    adds background image to full window

--- imGuiEx.cpp
+++ imGuiEx.cpp
@@ -2,12 +2,8 @@
 //
 
 #include "imGuiEx.h"
-#include "UseImGui.h"
-
-//#include "opencv2"
 
 using namespace std;
-//using namespace cv;
 
 class CustomImGui : public UseImGui {
 public:
@@ -17,8 +13,8 @@ public:
 	static float f = 0.0f;
 	static int counter = 0;
 
-	ImGui::Begin("Joor thi Bolo Jai Shree Ram");
-	ImGui::Text("Man ma Ram!");
+	ImGui::Begin("Joor Se Bolo");
+	ImGui::Text(" Jai Shree Ram!");
 
 	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);   	// Edit 1 float using a slider from 0.0f to 1.0f
 
@@ -32,6 +28,7 @@ public:
 	ImGui::Text("counter = %d", counter);
 
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	
 
 	ImGui::End();
 	}
@@ -43,7 +40,10 @@ int main()
 
 	// Setup window
 	if (!glfwInit())
+	{
+		std::cout <<"Failed to initialise GLFW" << std::endl;
 		return 1;
+	}
 
 
 	// GL 3.0 + GLSL 130
@@ -53,15 +53,32 @@ int main()
 	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
+	GLFWmonitor* MyMonitor = glfwGetPrimaryMonitor();
 
 	// Create window with graphics context
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui - Example", NULL, NULL);
+	//GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui - Example", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(glfwGetVideoMode(glfwGetPrimaryMonitor())->width,
+										  glfwGetVideoMode(glfwGetPrimaryMonitor())->height,
+								   	"JAI SHREE RAM MAIN WINDOW",
+									NULL, NULL);
+	
 	if (window == NULL)
+	{
+		glfwTerminate();
 		return 1;
+	}
+
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
 	glfwSwapInterval(1); // Enable vsync
 
+	// Displaying Image
+	// 
+	if (gl3wInit())
+	{
+		std::cout << "Failed to initialize openGL" << std::endl;
+		return 1;
+	}
 
 	int screen_width, screen_height;
 	glfwGetFramebufferSize(window, &screen_width, &screen_height);
@@ -69,22 +86,93 @@ int main()
 
 	// There are two implementations of this: 1) UseImGui Class 2) CustomImGui Class
 	// Just comment one and uncomment other 
-	//UseImGui myimgui;
-	CustomImGui myimgui; 
+	UseImGui myimgui;
+	//CustomImGui myimgui; 
 	myimgui.Init(window, glsl_version);
+
+
+	int my_image_width = 0;
+	int my_image_height = 0;
+	GLuint my_image_texture = 0;
+	bool ret = LoadTexutreFromFile("D:\\ImGuiEx\\fruitImage.jpg", &my_image_texture, &my_image_width, &my_image_height);
+	if (!ret)
+	{
+		std::cout << "Unable to load the image file" << std::endl;
+		return 1;
+	}
+
+	// Initialize OpenGL rendering context
+
+	
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		myimgui.NewFrame();
+
 		myimgui.Update();
+		ImGui::Begin("OpenGL JSR Texture");
+		ImGui::Text("pointer = %p", my_image_texture);
+		ImGui::Text("size = %d X %d", my_image_width, my_image_height);
+		ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
+		ImGui::End();
+
+		ImGui::Begin("JSR Background Texture");
+		ImGui::Text("pointer = %p", my_image_texture);
+		ImGui::Text("size = %d X %d", my_image_width, my_image_height);
+		int win_width = glfwGetVideoMode(glfwGetPrimaryMonitor())->width;
+		int win_height = glfwGetVideoMode(glfwGetPrimaryMonitor())->height;
+		ImGui::GetBackgroundDrawList()->AddImage((void*)(intptr_t)my_image_texture, ImVec2(0,0), ImVec2(win_width,win_height), ImVec2(0,0), ImVec2(1,1));
+		ImGui::End();
+
+
 		myimgui.Render();
 		glfwSwapBuffers(window);
 	}
 
 	myimgui.Shutdown();
 
-
 	return 0;
 }
+
+bool LoadTexutreFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
+{
+
+	int image_width = 0;
+	int image_height = 0;
+	unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+
+	if (image_data == NULL)
+	{
+		return false;
+	}
+
+	// Create a openGL texture identifier
+	GLuint image_texture;
+	glGenTextures(1, &image_texture);
+	glBindTexture(GL_TEXTURE_2D, image_texture);
+
+	// Setup filtering 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
+
+
+	// Upload pixels into texture
+
+	#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
+	glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+	#endif
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+	stbi_image_free(image_data);
+
+	*out_texture = image_texture;
+	*out_width = image_width;
+	*out_height = image_height;
+
+	return true;
+}
+
--- imGuiEx.h
+++ imGuiEx.h
@@ -3,6 +3,14 @@
 
 #pragma once
 
-#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
 
-// TODO: Reference additional headers your program requires here.
+#include <iostream>
+#include "UseImGui.h"
+#include "stb_image.h"
+
+
+// Simple helper function to load image into a OpenGL texture with common settings
+
+bool LoadTexutreFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height);
+
